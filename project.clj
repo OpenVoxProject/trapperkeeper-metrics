f320097d06474f2270bc9ee1abbc14eac22ebc79
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-12-31 23:01:20 +0000

    Version 2.0.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/trapperkeeper-metrics "2.0.5"
+(defproject org.openvoxproject/trapperkeeper-metrics "2.0.6-SNAPSHOT"
   :description "Trapperkeeper Metrics Service"
   :url "http://github.com/openvoxproject/trapperkeeper-metrics"
   :license {:name "Apache License, Version 2.0"
