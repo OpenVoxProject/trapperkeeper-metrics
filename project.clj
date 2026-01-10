d35cac983d455b4677fe2d26a0993d7e27c6664b
Nick Burgan <13688219+nmburgan@users.noreply.github.com>
2026-01-09 17:53:06 -0800

    Update trapperkeeper-authorization version to 2.1.1
    For some reason, renovate isn't picking this one up at the moment

--- project.clj
+++ project.clj
@@ -38,7 +38,7 @@
 
                  [org.openvoxproject/kitchensink]
                  [org.openvoxproject/trapperkeeper]
-                 [org.openvoxproject/trapperkeeper-authorization "2.1.0"]
+                 [org.openvoxproject/trapperkeeper-authorization "2.1.1"]
                  [org.openvoxproject/ring-middleware "2.1.0"]
 
                  [cheshire "5.13.0"]
