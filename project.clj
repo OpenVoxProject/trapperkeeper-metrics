41f2e73e66e89c9f6593da7f89a3068d44c89539
Tim Meusel <tim@bastelfreak.de>
2026-04-24 13:26:38 +0200

    org.openvoxproject/trapperkeeper-authorization: Update 2.1.8->2.1.9
    Signed-off-by: Tim Meusel <tim@bastelfreak.de>

--- project.clj
+++ project.clj
@@ -36,7 +36,7 @@
                          [org.openvoxproject/ring-middleware "2.1.7"]
                          [org.openvoxproject/trapperkeeper "4.3.5"]
                          [org.openvoxproject/trapperkeeper "4.3.5" :classifier "test"]
-                         [org.openvoxproject/trapperkeeper-authorization "2.1.8"]
+                         [org.openvoxproject/trapperkeeper-authorization "2.1.9"]
                          [org.openvoxproject/trapperkeeper-webserver-jetty10 "1.1.5"]
                          [org.slf4j/slf4j-api "2.0.17"]
                          [prismatic/schema "1.4.1"]
